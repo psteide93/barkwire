2e9c08d164afb690d731bb879a9a2bcc5cf2f88d
Parnell <psteide@ford.com>
2022-05-29 13:35:30 -0400

    Initial Commit

---
+++ src/main/java/com/psteide/barkwirelesson/BarkwireApplication.java
@@ -0,0 +1,13 @@
+package com.psteide.barkwirelesson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarkwireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarkwireApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/psteide/barkwirelesson/BarkwireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psteide.barkwirelesson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarkwireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
